443389e880eea28eaa4be7ce90d404f6772bc4d5
BanceDev <lanceaborden@protonmail.com>
2026-02-23 14:57:32 -0500

    added lockscreen to config

--- src/config.h
+++ src/config.h
@@ -23,6 +23,7 @@
     {MODKEY, XK_Return, 0, {.cmd = build_argv("alacritty")}, TYPE_CMD},        \
         {MODKEY, XK_d, 0, {.cmd = build_argv("kitty -e oxicord")}, TYPE_CMD},  \
         {MODKEY, XK_w, 0, {.cmd = build_argv("brave")}, TYPE_CMD},             \
+        {MODKEY, XK_r, 0, {.cmd = build_argv("lockrs")}, TYPE_CMD},            \
         {MODKEY, XK_space, 0, {.cmd = build_argv("dmenu_run")}, TYPE_CMD},     \
         {MODKEY, XK_equal, 0, {.cmd = build_argv("volup")}, TYPE_CMD},         \
         {MODKEY, XK_minus, 0, {.cmd = build_argv("voldown")}, TYPE_CMD},       \
